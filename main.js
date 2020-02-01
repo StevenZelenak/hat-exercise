2793bddeaa4d145a1da322a4c1dcaca0f4e5c897
StevenZelenak <stevenmzelenak@gmail.com>
2020-02-01 13:54:44 -0800

    update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //array of house names
 const houseNames = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
 
+//empty array to store students
 const arrStudents = [];
 
-const randomize = (arr) =>{
+//pick random house form array
+const randomize = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
@@ -23,16 +25,17 @@ const callForm = () => {
     }
 }
 
+//prints card to the dom
 const printCard = () => {
 
-    getValue();
+    createStudent();
     let domString = '';
     for (let index = 0; index < arrStudents.length; index++) {
         domString += '<div class="col-4 my-3">';
         domString += '<div class="card-body border border-dark rounded text-center mx-2">';
         domString += `<h3 class="card-title">${arrStudents[index].name}</h3>`;
         domString += `<p class="card-text">${arrStudents[index].house}</p>`;
-        domString += '<button class="btn btn-outline-dark btn-sm mx-5 px-5 py-2 font-weight-bold rounded">Expel!</button>';
+        domString += `<button id="${arrStudents[index].id}" class="btn btn-outline-dark btn-sm mx-5 px-5 py-2 font-weight-bold rounded">Expel!</button>`;
         domString += '</div>';
         domString += '</div>';
     }
@@ -40,13 +43,20 @@ const printCard = () => {
     clearForm();
 }
 
-//grabs the info from the from and puts it somewhere then clears the value of the form
+const studentCounter = (arr) =>{
+    let idCounter = 0;
+    idCounter = idCounter + arr.length;
+    return idCounter;
+}
 
-
-//grabs value from the form
-const getValue = () => {
+//grabs value from the form and puts it onto the card
+const createStudent = () => {
     let objvalue = document.getElementById('studentName').value;
-    arrStudents.push({name: objvalue, house:randomize(houseNames)});
+    arrStudents.push({
+        id: studentCounter(arrStudents),
+        name: objvalue,
+        house: randomize(houseNames)
+    });
     return arrStudents;
 }
 
