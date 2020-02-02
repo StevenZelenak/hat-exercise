44bc4ec6ee41e4f00ed0d4f034895ab3c8b393fa
Steven Zelenak <stevenmzelenak@gmail.com>
2020-02-01 22:39:19 -0600

    updating expel cards

--- main.js
+++ main.js
@@ -25,39 +25,48 @@ const callForm = () => {
     }
 }
 
-//prints card to the dom
-const printCard = () => {
+//counter to give ids of object same ids as buttons
+const studentCounter = () => {
 
-    createStudent();
-    let domString = '';
-    for (let index = 0; index < arrStudents.length; index++) {
-        domString += '<div class="col-4 my-3">';
-        domString += '<div class="card-body border border-dark rounded text-center mx-2">';
-        domString += `<h3 class="card-title">${arrStudents[index].name}</h3>`;
-        domString += `<p class="card-text">${arrStudents[index].house}</p>`;
-        domString += `<button id="${arrStudents[index].id}" class="btn btn-outline-dark btn-sm mx-5 px-5 py-2 font-weight-bold rounded">Expel!</button>`;
-        domString += '</div>';
-        domString += '</div>';
-    }
-    outputDomString("cardCall", domString);
-    clearForm();
-}
+    let number = arrStudents.length + 1;
+    return number.toString();
 
-const studentCounter = (arr) =>{
-    let idCounter = 0;
-    idCounter = idCounter + arr.length;
-    return idCounter;
 }
 
 //grabs value from the form and puts it onto the card
 const createStudent = () => {
     let objvalue = document.getElementById('studentName').value;
     arrStudents.push({
-        id: studentCounter(arrStudents),
+        id: studentCounter(),
         name: objvalue,
         house: randomize(houseNames)
     });
-    return arrStudents;
+    
+    
+    printCard(arrStudents);
+    console.log(arrStudents);
+}
+
+//prints card to the dom
+const printCard = (arr) => {
+
+    
+    
+    let domString = '';
+    for (let index = 0; index < arr.length; index++) {
+        domString += '<div class="col-4 my-3">';
+        domString += '<div class="card-body border border-dark rounded text-center mx-2">';
+        domString += `<h3 class="card-title">${arr[index].name}</h3>`;
+        domString += `<p class="card-text">${arr[index].house}</p>`;
+        domString += `<button id="${arr[index].id}" class="btn btn-outline-dark btn-sm mx-5 px-5 py-2 font-weight-bold rounded">Expel!</button>`;
+        domString += '</div>';
+        domString += '</div>';
+
+    }
+    outputDomString("cardCall", domString);
+    clearForm();
+    eventButtonLoop();
+    
 }
 
 //clears the value of the form
@@ -65,10 +74,32 @@ const clearForm = () => {
     document.getElementById('studentName').value = '';
 }
 
+//creates a button with a id that is equal to every object created
+const eventButtonLoop = () => {
+
+    for (let i = 0; i < arrStudents.length; i++) {
+        document.getElementById(arrStudents[i].id).addEventListener('click', expelStudent);
+    }
+}
+
+//removes a object from the array
+const expelStudent = (e) => {
+
+    for (let i = 0; i < arrStudents.length; i++) {
+        if (e.target.id === arrStudents[i].id) {
+            arrStudents.splice(i, 1);
+        }
+
+    }
+
+    printCard(arrStudents);
+
+}
+
 //where the events are stored
 const events = () => {
 
-    document.getElementById('btnSubmit').addEventListener('click', printCard);
+    document.getElementById('btnSubmit').addEventListener('click', createStudent);
     document.getElementById('formCallButton').addEventListener('click', callForm);
 }
 
